af1a8b99fe3d79fef48c70ad5ba41ed40ff4271f
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2022-06-22 11:15:14 +0900

    Update index.js

--- script/index.js
+++ script/index.js
@@ -4,12 +4,12 @@ const $sections = [...document.querySelectorAll("section")];
 function classHandle(e, listArr, listClass) {
     listArr.map(t => {
         t.classList.remove(listClass);
-    })
+    });
     e.classList.add(listClass);
 };
 
 for (let i = 0; i < $btns.length; i++) {
-    $btns[i].addEventListener("click", (e) => {
+    $btns[i].addEventListener("click", e => {
         e.preventDefault();
         classHandle(e.currentTarget, $btns, "handle1");
         classHandle($sections[i], $sections, "handle2");
